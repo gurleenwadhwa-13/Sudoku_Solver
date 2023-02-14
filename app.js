d7718c7f5fb053f243deeca5fbef644806bd7a0d
Gurleen <gurleenwadhwa@dal.ca>
2023-02-13 22:58:46 -0400

    Created grid and started JS code

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //COde comes here
+    const width = 10;
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startStopBttn = document.querySelector('#start-button')
+
+    
+    
+
+
+
+
+
+
+
 })
