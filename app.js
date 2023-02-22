9106b75e00314f4c40e3c1ecefc9951d7a8963f4
Gurleen <gurleenwadhwa@dal.ca>
2023-02-22 15:22:31 -0400

    Created the grid

--- app.js
+++ app.js
@@ -6,8 +6,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const startStopBttn = document.querySelector('#start-button')
 
     
-    
+    //The tetriminoes
+    const lTetrimino = [
+        [1, width, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width, width*2+1, width*2],
+        [width, width*2, width]
+    ]
 
+    const zTetriminoes = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0,width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const tTetriminoes = [
+        [1, width, width+1, width*2+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetrimino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width*2, width*3], 
+        [1, width+1, width*2+1, width*3+1, width*3+1],
+        [width, width*1, width+2, width*3]
+    ]
+
+    const theTetriminoes = [lTetrimino, zTetriminoes, tTetriminoes, oTetromino, iTetrimino]
+
+    let currentPosition = 4;
+    let current = theTetriminoes[0][0];
 
 
 
