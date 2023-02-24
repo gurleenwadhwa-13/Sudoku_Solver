61e8b9a9ac61b5999e8c567221b8f4adef4c3f32
Gurleen Wadhwa <71890934+gurleenwadhwa-13@users.noreply.github.com>
2023-02-24 16:56:57 -0400

    Added Left and Right functionality (#3)
    * Completed the tetris shapes and their movement
    
    * added left and right moving commands

---
+++ script.js
@@ -0,0 +1,156 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const width = 10;
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startStopBttn = document.querySelector('#start-button')
+
+    
+    //The tetriminoes
+    const lTetrimino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width]
+    ]
+
+    const zTetriminoes = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0,width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const tTetriminoes = [
+        [1, width, width+1, width*2+1],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetrimino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width*2, width*3], 
+        [1, width+1, width*2+1, width*3+1, width*3+1],
+        [width, width*1, width+2, width*3]
+    ]
+
+    const theTetriminoes = [lTetrimino, zTetriminoes, tTetriminoes, oTetromino, iTetrimino]
+
+    let currentPosition = 4
+    let currentRotation = 2
+
+    //randomly pick a tetrimino and its first rotation. 
+    let random = Math.floor(Math.random()*theTetriminoes.length)
+    let current = theTetriminoes[random][currentRotation]
+
+    //draw the rotation from a random tetrimino
+    function draw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    function undraw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    //making the tetrimino move down every second. 
+    timerID = setInterval(moveDown, 1000)
+
+    //assigning functions to keyCodes
+    function control(e) {
+        if(e.keyCode === 37){
+            moveLeft()
+        }else if(e.keyCode === 38){
+            //rotate
+        }else if(e.keyCode === 39){
+            moveRight()
+        }else if(e.keyCode === 40){
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
+
+
+    function moveDown(){
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('occupied'))){
+            current.forEach(index => squares[currentPosition + index].classList.add("occupied"))
+
+            //start a new tetrimino sequence from here on
+            random = Math.floor(Math.random() * theTetriminoes.length)
+            current = theTetriminoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    //Moving our tetrimino left, unless there is a blockage or end of grid has been reached. 
+    function moveLeft(){
+        undraw()
+        const isAtLeftPos = current.some(index => (currentPosition+index) % width == 0)
+
+        //Allowing the tetrimino to go left if its current position is not the extreme left
+        if(!isAtLeftPos) currentPosition -= 1
+            
+        //if the last row is reached/ the current blocks are already occupied with existing tetrimino, then 
+        if(current.some(index => squares[currentPosition + index].classList.contains('occupied'))){
+            currentPosition += 1
+        }
+        draw()
+    }
+
+
+    function moveRight(){
+        undraw()
+        const isAtRightPos = current.some(index => (currentPosition+index) % width == width-1)
+
+        //Allowing the tetrimino to go right if its current position is not the extreme right
+        if(!isAtRightPos) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition+index].classList.contains('occupied'))){
+            currentPosition -=1
+        }
+        draw()
+    }
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+})
